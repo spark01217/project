eec6ab85d09da2cd7f8f1e2488b08476fcb2306d
Zhipeng Zhu <zpzhu@cs.uchicago.edu>
2017-02-25 15:47:02 -0600

    msg

--- cta.py
+++ cta.py
@@ -13,7 +13,7 @@ dta = dta.groupby('date').head(10)
 # dta.to_csv('/home/zpzhu/cs122-win-17-zpzhu/pa3/ui/project/cta_data.csv')
 def calctot(df):
     #delete columns
-    df = df.drop(['station_id', 'stationname', 'date', 'daytype'], axis=1)
+    df = df.drop(['station_id', 'stationname', 'date', 'daytype'], axis = 1)
     #append sum row, ignoring non-numeric column metrics
     return df.append(df.sum(numeric_only=True), ignore_index=True)
 
@@ -22,7 +22,9 @@ nc =  dta.groupby('date').apply(calctot).reset_index()
 #delete old index column
 nc = nc.drop(['level_1'], axis=1)
 #add new column onto the dataframe
-dta['rides1'] = pd.Series(nc, index = dta['date'])
+
+# dta['rides1'] = pd.Series(nc, index = dta['date'])
+
 #fill NaN to value tot
 dta['date'] = dta['date'].fillna('tot')
 
