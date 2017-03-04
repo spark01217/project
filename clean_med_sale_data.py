89e81741a2ad782cb3bf88cfeaf20547c26d4790
Sangwoo Park <spark01217@cs.uchicago.edu>
2017-03-04 16:25:46 -0600

    matrix

--- clean_med_sale_data.py
+++ clean_med_sale_data.py
@@ -31,7 +31,8 @@ combined.loc[205] = [2013, 58500, 54]
 combined.loc[206] = [2014, 52500, 54]
 combined.loc[207] = [2015, 79000, 54]
 
-combined = combined.pivot_table("value", "community", "date")
-combined.columns = ["2012_Price", "2013_Price", "2014_Price", "2015_Price", "2016_Price"]
+combined = combined.groupby(['date', 'community'])['value'].mean()
+combined = combined.to_frame()
+combined = pd.DataFrame(combined.to_records())
 combined = combined.astype(int)
-combined.to_csv("final_med_price_data.csv")
+combined.to_csv("final_data/final_med_price_data.csv", index=False)
