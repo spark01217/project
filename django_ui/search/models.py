9dad517cf0ea6a46bbfa27c2551e4f4a676e4411
Edward Francis Hayes <edwardhayes@Edwards-MacBook-Pro-2.local>
2017-03-08 12:58:45 -0600

    all

--- django_ui/search/models.py
+++ django_ui/search/models.py
@@ -12,16 +12,3 @@ class Neighborhood(models.Model):
 
     def __str__(self):
         return str(self.code) + ', ' + self.name
-
-
-class TestingNeighborhood(models.Model):
-    code = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=250)
-    price = models.IntegerField()
-    crime = models.IntegerField()
-    school = models.IntegerField()
-    income = models.IntegerField()
-    cta = models.IntegerField()
-
-    def __str__(self):
-        return str(self.code) + ', ' + self.name
